stavro/arc

Clean up all temp files created during processing and storage

lib/arc/file.ex
@@ -1,5 +1,5 @@
 defmodule Arc.File do
-  defstruct [:path, :file_name, :binary]
+  defstruct [:path, :file_name, :binary, :tempfile?]
 
   def generate_temporary_path(file \\ nil) do
     extension = Path.extname((file && file.path) || "")
@@ -18,7 +18,7 @@ defmodule Arc.File do
     filename = Path.basename(uri.path)
 
     case save_file(uri, filename) do
-      {:ok, local_path} -> %Arc.File{path: local_path, file_name: filename}
+      {:ok, local_path} -> %Arc.File{path: local_path, file_name: filename, tempfile?: true}
       :error -> {:error, :invalid_file_path}
     end
 end
@@ -33,6 +33,7 @@ end
 
   def new(%{filename: filename, binary: binary}) do
     %Arc.File{binary: binary, file_name: Path.basename(filename)}
+    |> write_binary()
   end
 
   # Accepts a map conforming to %Plug.Upload{} syntax
@@ -43,8 +44,6 @@ end
     end
   end
 
-  def ensure_path(file = %{path: path}) when is_binary(path), do: file
-  def ensure_path(file = %{binary: binary}) when is_binary(binary), do: write_binary(file)
 
   defp write_binary(file) do
     path = generate_temporary_path(file)
@@ -52,7 +51,8 @@ end
 
     %__MODULE__{
       file_name: file.file_name,
-      path: path
+      path: path,
+      tempfile?: true
     }
   end
 

lib/arc/processor.ex
@@ -12,6 +12,6 @@ defmodule Arc.Processor do
   end
 
   defp apply_transformation(file, {cmd, conversion}) do
-    Arc.Transformations.Convert.apply(cmd, Arc.File.ensure_path(file), conversion)
+    Arc.Transformations.Convert.apply(cmd, file, conversion)
   end
 end

lib/arc/transformations/convert.ex
@@ -6,9 +6,10 @@ defmodule Arc.Transformations.Convert do
 
     ensure_executable_exists!(program)
 
-    case System.cmd(program, args_list(args), stderr_to_stdout: true) do
+    result = System.cmd(program, args_list(args), stderr_to_stdout: true)
+    case result do
       {_, 0} ->
-        {:ok, %Arc.File{file | path: new_path}}
+        {:ok, %Arc.File{file | path: new_path, tempfile?: true}}
       {error_message, _exit_code} ->
         {:error, error_message}
     end

test/storage/local_test.exs
@@ -3,11 +3,14 @@ defmodule ArcTest.Storage.Local do
   @img "test/support/image.png"
   @badimg "test/support/invalid_image.png"
 
-  setup_all do
+  setup do
     File.mkdir_p("arctest/uploads")
+    File.mkdir_p("arctest/tmp")
+    System.put_env("TMPDIR", "arctest/tmp")
 
     on_exit fn ->
       File.rm_rf("arctest/uploads")
+      File.rm_rf("arctest/tmp")
     end
   end
 
@@ -67,5 +70,24 @@ defmodule ArcTest.Storage.Local do
 
     assert !File.exists?("arctest/uploads/original-#{filename}")
     assert !File.exists?("arctest/uploads/1/thumb-#{filename}")
+  end
+
+  test "temp files from processing are cleaned up" do
+    filepath = @img
+    DummyDefinition.store(filepath)
+    assert length(File.ls!("arctest/tmp")) == 0
+  end
+
+  test "temp files from handling binary data are cleaned up" do
+    filepath = @img
+    filename = "image.png"
+    DummyDefinition.store(%{binary: File.read!(filepath), filename: filename})
+    assert File.exists?("arctest/uploads/original-#{filename}")
+    assert length(File.ls!("arctest/tmp")) == 0
+  end
+
+  test "temp files from handling remote URLs are cleaned up" do
+    DummyDefinition.store("https://www.google.com/favicon.ico")
+    assert length(File.ls!("arctest/tmp")) == 0
   end
 end

elixir-waffle/waffle

lib/waffle/file.ex
@@ -1,5 +1,5 @@
 defmodule Waffle.File do
-  defstruct [:path, :file_name, :binary]
+  defstruct [:path, :file_name, :binary, :tempfile?]
 
   def generate_temporary_path(file \\ nil) do
     extension = Path.extname((file && file.path) || "")
@@ -18,7 +18,7 @@ defmodule Waffle.File do
     filename = Path.basename(uri.path)
 
     case save_file(uri, filename) do
-      {:ok, local_path} -> %Waffle.File{path: local_path, file_name: filename}
+      {:ok, local_path} -> %Waffle.File{path: local_path, file_name: filename, tempfile?: true}
       :error -> {:error, :invalid_file_path}
     end
 end
@@ -33,6 +33,7 @@ end
 
   def new(%{filename: filename, binary: binary}) do
     %Waffle.File{binary: binary, file_name: Path.basename(filename)}
+    |> write_binary()
   end
 
   # Accepts a map conforming to %Plug.Upload{} syntax
@@ -43,8 +44,6 @@ end
     end
   end
 
-  def ensure_path(file = %{path: path}) when is_binary(path), do: file
-  def ensure_path(file = %{binary: binary}) when is_binary(binary), do: write_binary(file)
 
   defp write_binary(file) do
     path = generate_temporary_path(file)
@@ -52,7 +51,8 @@ end
 
     %__MODULE__{
       file_name: file.file_name,
-      path: path
+      path: path,
+      tempfile?: true
     }
   end
 

lib/waffle/processor.ex
@@ -12,6 +12,6 @@ defmodule Waffle.Processor do
   end
 
   defp apply_transformation(file, {cmd, conversion}) do
-    Waffle.Transformations.Convert.apply(cmd, Waffle.File.ensure_path(file), conversion)
+    Waffle.Transformations.Convert.apply(cmd, file, conversion)
   end
 end

lib/waffle/transformations/convert.ex
@@ -6,9 +6,10 @@ defmodule Waffle.Transformations.Convert do
 
     ensure_executable_exists!(program)
 
-    case System.cmd(program, args_list(args), stderr_to_stdout: true) do
+    result = System.cmd(program, args_list(args), stderr_to_stdout: true)
+    case result do
       {_, 0} ->
-        {:ok, %Waffle.File{file | path: new_path}}
+        {:ok, %Waffle.File{file | path: new_path, tempfile?: true}}
       {error_message, _exit_code} ->
         {:error, error_message}
     end

test/storage/local_test.exs
@@ -3,11 +3,14 @@ defmodule WaffleTest.Storage.Local do
   @img "test/support/image.png"
   @badimg "test/support/invalid_image.png"
 
-  setup_all do
+  setup do
     File.mkdir_p("waffletest/uploads")
+    File.mkdir_p("waffletest/tmp")
+    System.put_env("TMPDIR", "waffletest/tmp")
 
     on_exit fn ->
       File.rm_rf("waffletest/uploads")
+      File.rm_rf("waffletest/tmp")
     end
   end
 
@@ -73,5 +76,24 @@ defmodule WaffleTest.Storage.Local do
 
     assert !File.exists?("waffletest/uploads/original-#{filename}")
     assert !File.exists?("waffletest/uploads/1/thumb-#{filename}")
+  end
+
+  test "temp files from processing are cleaned up" do
+    filepath = @img
+    DummyDefinition.store(filepath)
+    assert length(File.ls!("waffletest/tmp")) == 0
+  end
+
+  test "temp files from handling binary data are cleaned up" do
+    filepath = @img
+    filename = "image.png"
+    DummyDefinition.store(%{binary: File.read!(filepath), filename: filename})
+    assert File.exists?("waffletest/uploads/original-#{filename}")
+    assert length(File.ls!("waffletest/tmp")) == 0
+  end
+
+  test "temp files from handling remote URLs are cleaned up" do
+    DummyDefinition.store("https://www.google.com/favicon.ico")
+    assert length(File.ls!("waffletest/tmp")) == 0
   end
 end