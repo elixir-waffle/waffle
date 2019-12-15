273d59840cc2d3f006d96ccbbe93de7f01d62ad7
Boris Kuznetsov <achempion@gmail.com>
2019-12-15 09:42:34 +0300

    documentation for definition module

--- lib/waffle/definition.ex
+++ lib/waffle/definition.ex
@@ -1,4 +1,26 @@
 defmodule Waffle.Definition do
+  @moduledoc ~S"""
+  Defines uploader to manage files.
+
+      defmodule Avatar do
+        use Waffle.Definition
+      end
+
+  Consists of several components to manage different parts of file
+  managing process.
+
+    * `Waffle.Definition.Versioning`
+
+    * `Waffle.Definition.Storage`
+
+    * `Waffle.Actions.Store`
+
+    * `Waffle.Actions.Delete`
+
+    * `Waffle.Actions.Url`
+
+  """
+
   defmacro __using__(_options) do
     quote do
       use Waffle.Definition.Versioning
