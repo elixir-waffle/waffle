cf1927e11b739767efc9fef67941ee19d07094e8
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2020-06-17 14:03:40 -0400

    move functions defined in before_compile to using as overridable
    The code generated in Definition includes default implementations of
    functions which are supposed to be overridable. The problem is that if
    the overridable implementations do pattern matching in the function head
    and those patterns fail, instead of an error being raised, the default
    implementation generated by Waffle is used, which hides the error and
    can result in unexpected behavior (in my case, it was the ACL being
    incorrectly set).
    
    This change makes those functions overridable like the others, which
    preserves the default behavior, but behaves as expected when a custom
    implementation is provided.

--- lib/waffle/definition/storage.ex
+++ lib/waffle/definition/storage.ex
@@ -90,8 +90,14 @@ defmodule Waffle.Definition.Storage do
       def default_url(version, _), do: default_url(version)
       def default_url(_), do: nil
       def __storage, do: Application.get_env(:waffle, :storage, Waffle.Storage.S3)
+      def acl(_, _), do: @acl
+      def s3_object_headers(_, _), do: []
+      def async, do: @async
 
-      defoverridable storage_dir_prefix: 0,
+      defoverridable acl: 2,
+                     async: 0,
+                     s3_object_headers: 2,
+                     storage_dir_prefix: 0,
                      storage_dir: 2,
                      filename: 2,
                      validate: 1,
@@ -100,16 +106,6 @@ defmodule Waffle.Definition.Storage do
                      __storage: 0,
                      bucket: 0,
                      asset_host: 0
-
-      @before_compile Waffle.Definition.Storage
-    end
-  end
-
-  defmacro __before_compile__(_env) do
-    quote do
-      def acl(_, _), do: @acl
-      def s3_object_headers(_, _), do: []
-      def async, do: @async
     end
   end
 end
