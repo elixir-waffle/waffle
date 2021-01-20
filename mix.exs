5e6c671f51bf14864d6f5125141456cd28f7e791
c4710n <c4710n@users.noreply.github.com>
2021-01-20 17:13:36 +0800

    Upgrade deps for fixing compile warning in Elixir 1.11

--- mix.exs
+++ mix.exs
@@ -49,8 +49,8 @@ defmodule Waffle.Mixfile do
       {:hackney, "~> 1.9"},
 
       # If using Amazon S3
-      {:ex_aws, "~> 2.1.2", optional: true},
-      {:ex_aws_s3, "~> 2.0", optional: true},
+      {:ex_aws, "~> 2.1", optional: true},
+      {:ex_aws_s3, "~> 2.1", optional: true},
       {:sweet_xml, "~> 0.6", optional: true},
 
       # Test
