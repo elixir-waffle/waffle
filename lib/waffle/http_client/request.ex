3945a3ba6cdeee87747d790170fd7d4eb5fb3bc6
Boris Kuznetsov <me@achempion.com>
2026-08-16 22:12:31 +0200

    Set max body length to 50 MiB by default

--- lib/waffle/http_client/request.ex
+++ lib/waffle/http_client/request.ex
@@ -15,7 +15,7 @@ defmodule Waffle.HTTPClient.Request do
           max_retries: 3,
           receive_timeout_ms: 5_000,
           connect_timeout_ms: 10_000,
-          max_body_length_bytes: 50 * 1024,
+          max_body_length_bytes: 50 * 1024 * 1024,
           backoff_factor_ms: 1_000,
           backoff_max_ms: 30_000
         ]
@@ -43,7 +43,7 @@ defmodule Waffle.HTTPClient.Request do
     max_retries: 3,
     receive_timeout_ms: 5_000,
     connect_timeout_ms: 10_000,
-    max_body_length_bytes: 50 * 1024,
+    max_body_length_bytes: 50 * 1024 * 1024,
     backoff_factor_ms: 1_000,
     backoff_max_ms: 30_000
   ]
